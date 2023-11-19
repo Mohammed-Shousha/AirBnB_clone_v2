93f1f9e7e1e5e1602a0886cf086d2b9adf6326c5
Mohammed Shousha <mshousha29@gmail.com>
2023-11-19 20:42:20 +0200

    place

--- models/place.py
+++ models/place.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+"""Place class module"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+class Place(BaseModel, Base):
+    """
+    Place class
+    Attributes:
+        city_id (str): the city id
+        user_id (str): the user id
+        name (str): the name of the place
+        description (str): the description of the place
+        number_rooms (int): the number of rooms
+        number_bathrooms (int): the number of bathrooms
+        max_guest (int): the max number of guests
+        price_by_night (int): the price by night
+        latitude (float): the latitude of the place
+        longitude (float): the longitude of the place
+        amenity_ids (list): list of amenity ids
+    """
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
