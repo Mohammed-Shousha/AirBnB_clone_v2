b18f5e4a0d177f3b44c3ce328eac5d6fa006eef7
root <mshousha29@gmail.com>
2023-12-07 06:40:53 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,32 +16,18 @@ def do_deploy(archive_path):
 
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
+    file_path = "/data/web_static/releases/{}".format(name)
+    tmp = "/tmp/" + name
 
-
-    if put(archive_path, "/tmp/{}".format(file)).failed:
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(file_path))
+        run("tar -xzf {} -C {}/".format(tmp, file_name))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(file_name, file_name))
+        run("rm -rf {}/web_static".format(file_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(file_name))
+        return True
+    except:
         return False
-    if run("rm -rf /data/web_static/releases/{}/"
-           .format(name)).failed:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/"
-           .format(name)).failed:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-           .format(file, name)).failed:
-        return False
-    if run("rm /tmp/{}".format(file)).failed:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static"
-           .format(name)).failed:
-        return False
-    if run("rm -rf /data/web_static/current").failed:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-           .format(name)).failed:
-        return False
-
-    print("New version deployed!")
-    return True
